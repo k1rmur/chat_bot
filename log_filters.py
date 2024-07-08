babb5431f5e98053e97a55e3d5144d885c5fac4d
alexeybur <whitefrog721@gmail.com>
2024-07-08 12:15:37 +0300

    Logging

--- log_filters.py
+++ log_filters.py
@@ -3,7 +3,7 @@ import logging
 class InfoLogFilter(logging.Filter):
     def filter(self, record):
         return record.levelname == 'INFO'
-    
+
 
 class ExceptionLogFilter(logging.Filter):
     def filter(self, record):
