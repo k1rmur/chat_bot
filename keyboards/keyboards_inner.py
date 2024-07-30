74da243dd99e697e7aad77e1617999be3e1ae89c
alexeybur <schneider.mark14@gmail.com>
2024-07-30 13:04:17 +0700

    inner keyboard

--- keyboards/keyboards_inner.py
+++ keyboards/keyboards_inner.py
@@ -1,12 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def general_menu():
-    btn_vod_obj = KeyboardButton("Виртуальный собеседник")
-    btn_gouslugi = KeyboardButton("Электронный протокол")
-    btn_struct_rosvodres = KeyboardButton("Регламенты ПКИ")
-    btn_faq = KeyboardButton("ГосУслуги")
-    btn_chs = KeyboardButton("Оперативная информация о водохозяйственной обстановке")
-    btn_priem = KeyboardButton("Обратная связь")
+    btn_vod_obj = KeyboardButton(text="Виртуальный собеседник")
+    btn_gouslugi = KeyboardButton(text="Электронный протокол")
+    btn_struct_rosvodres = KeyboardButton(text="Регламенты ПКИ")
+    btn_faq = KeyboardButton(text="ГосУслуги")
+    btn_chs = KeyboardButton(text="Оперативная информация о водохозяйственной обстановке")
+    btn_priem = KeyboardButton(text="Обратная связь")
     markup = ReplyKeyboardMarkup(
         keyboard= [[btn_vod_obj], [btn_gouslugi], [btn_chs], [btn_struct_rosvodres], [btn_priem], [btn_faq]],
         resize_keyboard=True
@@ -14,12 +14,12 @@ def general_menu():
     return markup 
 
 def gosuslugi_menu():
-    btn_doc1 = KeyboardButton('''Водный реестр''')
-    btn_doc2 = KeyboardButton('''Право пользования''')
-    btn_doc3 = KeyboardButton('''Договоры''')
-    btn_doc5 = KeyboardButton('''Земельный участок''')
-    btn_doc4 = KeyboardButton('''Допустимые нормы''')
-    btn_back = KeyboardButton('''Назад''')
+    btn_doc1 = KeyboardButton(text='''Водный реестр''')
+    btn_doc2 = KeyboardButton(text='''Право пользования''')
+    btn_doc3 = KeyboardButton(text='''Договоры''')
+    btn_doc5 = KeyboardButton(text='''Земельный участок''')
+    btn_doc4 = KeyboardButton(text='''Допустимые нормы''')
+    btn_back = KeyboardButton(text='''Назад''')
     markup = ReplyKeyboardMarkup(
         keyboard= [[btn_doc1], [btn_doc2], [btn_doc3], [btn_doc4], [btn_doc5], [btn_back]],
         resize_keyboard=True
@@ -27,12 +27,12 @@ def gosuslugi_menu():
     return markup
 
 def reglament_menu():
-    btn_doc1 = KeyboardButton('''Бюджетные сметы''')
-    btn_doc2 = KeyboardButton('''Субвенции''')
-    btn_doc3 = KeyboardButton('''Субсидии на иные цели''')
-    btn_doc5 = KeyboardButton('''Капитальный ремонт''')
-    btn_doc4 = KeyboardButton('''Капитальное строительство''')
-    btn_back = KeyboardButton('''Назад''')
+    btn_doc1 = KeyboardButton(text='''Бюджетные сметы''')
+    btn_doc2 = KeyboardButton(text='''Субвенции''')
+    btn_doc3 = KeyboardButton(text='''Субсидии на иные цели''')
+    btn_doc5 = KeyboardButton(text='''Капитальный ремонт''')
+    btn_doc4 = KeyboardButton(text='''Капитальное строительство''')
+    btn_back = KeyboardButton(text='''Назад''')
     markup = ReplyKeyboardMarkup(
         keyboard= [[btn_doc1], [btn_doc2], [btn_doc3], [btn_doc4], [btn_doc5], [btn_back]],
         resize_keyboard=True
