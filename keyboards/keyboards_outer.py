c566b9ece6419b3f5e6849fda8ecf23a90dd2da2
alexeybur <schneider.mark14@gmail.com>
2024-07-30 12:34:14 +0700

    add text argument to buttons

--- keyboards/keyboards_outer.py
+++ keyboards/keyboards_outer.py
@@ -2,12 +2,12 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 def general_menu():
-    btn_vod_obj = KeyboardButton("Виртуальный собеседник")
-    btn_gouslugi = KeyboardButton("ГосУслуги")
-    btn_struct_rosvodres = KeyboardButton("Структура Росводресурсов")
-    btn_faq = KeyboardButton("Обратная связь")
-    btn_chs = KeyboardButton("Информация о ЧС")
-    btn_priem = KeyboardButton("Прием граждан")
+    btn_vod_obj = KeyboardButton(text="Виртуальный собеседник")
+    btn_gouslugi = KeyboardButton(text="ГосУслуги")
+    btn_struct_rosvodres = KeyboardButton(text="Структура Росводресурсов")
+    btn_faq = KeyboardButton(text="Обратная связь")
+    btn_chs = KeyboardButton(text="Информация о ЧС")
+    btn_priem = KeyboardButton(text="Прием граждан")
     markup = ReplyKeyboardMarkup(
         keyboard= [[btn_vod_obj], [btn_gouslugi], [btn_chs], [btn_struct_rosvodres], [btn_priem], [btn_faq]],
         resize_keyboard=True
@@ -15,12 +15,12 @@ def general_menu():
     return markup 
 
 def gosuslugi_menu():
-    btn_doc1 = KeyboardButton('''Водный реестр''')
-    btn_doc2 = KeyboardButton('''Право пользования''')
-    btn_doc3 = KeyboardButton('''Договоры''')
-    btn_doc5 = KeyboardButton('''Земельный участок''')
-    btn_doc4 = KeyboardButton('''Допустимые нормы''')
-    btn_back = KeyboardButton('''Назад''')
+    btn_doc1 = KeyboardButton(text='''Водный реестр''')
+    btn_doc2 = KeyboardButton(text='''Право пользования''')
+    btn_doc3 = KeyboardButton(text='''Договоры''')
+    btn_doc5 = KeyboardButton(text='''Земельный участок''')
+    btn_doc4 = KeyboardButton(text='''Допустимые нормы''')
+    btn_back = KeyboardButton(text='''Назад''')
     markup.add(btn_doc1, btn_doc2, btn_doc3, btn_doc4, btn_doc5, btn_back)
     markup = ReplyKeyboardMarkup(
         keyboard= [[btn_doc1], [btn_doc2], [btn_doc3], [btn_doc4], [btn_doc5], [btn_back]],
