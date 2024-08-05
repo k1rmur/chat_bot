a40bdba7d6b82bae2eae20fcbbe5dd396bd1759f
rosvodres <agrngabena@gmail.com>
2024-08-05 09:14:43 +0000

    upd script

--- run_bot_test.sh
+++ run_bot_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
- 
+
+git checkout develop
+git pull && \ 
 docker compose -f docker-compose_test.yml down
 docker compose -f docker-compose_test.yml up -d --build && \
 docker network connect protocol_model favr_outer_test
