17717293e88bca6460103dec1f43003d2cdec65f
alexeybur <schneider.mark14@gmail.com>
2024-08-06 19:37:40 +0500

    change structure

--- run_bot_test.sh
+++ run_bot_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git checkout develop
-git pull && \ 
+git pull && \
 docker compose -f docker-compose_test.yml down
 docker compose -f docker-compose_test.yml up -d --build && \
 docker network connect protocol_model favr_outer_test
