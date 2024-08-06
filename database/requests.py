25f28aae190573e5ef9a51425ee2ab19bae406e5
alexeybur <schneider.mark14@gmail.com>
2024-08-06 17:58:32 +0500

    remove chat id, same as id

--- database/requests.py
+++ database/requests.py
@@ -8,12 +8,11 @@ class Database:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def add_user(self, id: int, chat_id: int, username: str) -> None:
+    async def add_user(self, id: int, username: str) -> None:
         user = await self.session.get(User, id)
         if not user:
             user = User(
                 id=id,
-                chat_id=chat_id,
                 username=username,
             )
             self.session.add(user)
@@ -21,7 +20,7 @@ class Database:
 
 
     async def get_chat_ids(self):
-        stmt = select(User.chat_id)
+        stmt = select(User.id)
         result = await self.session.execute(stmt)
         print(result)
         return [user for user in result.all()]
