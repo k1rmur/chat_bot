0896170975cc5ab41ba2691a7c8479b55ffc42d6
Mark Schneider <schneider.mark14@gmail.com>
2024-12-25 09:12:41 +0300

    [upd] Actualize make_embeddings

--- bot/make_embeddings.py
+++ bot/make_embeddings.py
@@ -1,17 +1,14 @@
 import os
-from optparse import OptionParser
 
 import chromadb
 import torch
 from llama_index.core import (Settings, SimpleDirectoryReader, StorageContext,
                               VectorStoreIndex)
-from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.node_parser import TokenTextSplitter
 from llama_index.core.storage.docstore import SimpleDocumentStore
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.retrievers.bm25 import BM25Retriever
 from llama_index.vector_stores.chroma import ChromaVectorStore
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from llama_index.core.node_parser import LangchainNodeParser
 
 ABS_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -24,39 +21,27 @@ device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 print("DEVICE:", device)
 embeddings = HuggingFaceEmbedding(model_name="intfloat/multilingual-e5-small", device=device)
 Settings.embed_model = embeddings
-Settings.chunk_size = 256
-Settings.chunk_overlap = 64
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     try:
-        os.remove(f'{DB_DIR}/chroma.sqlite3')
+        os.remove(f"{DB_DIR}/chroma.sqlite3")
     except:
         pass
 
     documents = reader.load_data()
-    parser = LangchainNodeParser(
-        RecursiveCharacterTextSplitter(
-            chunk_size=1024,
-            chunk_overlap=256,
-        )
-    )
+    parser = TokenTextSplitter(chunk_size=1024, chunk_overlap=256)
     nodes = parser.get_nodes_from_documents(documents)
     print(nodes)
     docstore = SimpleDocumentStore()
     docstore.add_documents(nodes)
 
-    bm25_retriever = BM25Retriever.from_defaults(
-        docstore=docstore, similarity_top_k=5
-    )
+    bm25_retriever = BM25Retriever.from_defaults(docstore=docstore, similarity_top_k=5)
 
     db = chromadb.PersistentClient(path=DB_DIR)
     chroma_collection = db.create_collection("embeddings")
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    storage_context = StorageContext.from_defaults(
-        vector_store=vector_store
-    )
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
     vector_index = VectorStoreIndex.from_documents(
         documents, storage_context=storage_context
@@ -69,26 +54,21 @@ else:
         docstore = SimpleDocumentStore.from_persist_path(f"{DB_DIR}/docstore")
     except Exception:
         documents = reader.load_data()
-        parser = SentenceSplitter()
+        parser = TokenTextSplitter(chunk_size=1024, chunk_overlap=256)
         nodes = parser.get_nodes_from_documents(documents)
         docstore = SimpleDocumentStore()
         docstore.add_documents(nodes)
         docstore.persist(f"{DB_DIR}/docstore")
-
 
     db = chromadb.PersistentClient(path=DB_DIR)
 
     chroma_collection = db.get_or_create_collection("embeddings")
 
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    storage_context = StorageContext.from_defaults(
-        vector_store=vector_store
-    )
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
     vector_index = VectorStoreIndex.from_vector_store(
         vector_store, storage_context=storage_context
     )
 
-    bm25_retriever = BM25Retriever.from_defaults(
-        docstore=docstore, similarity_top_k=5
-    )
+    bm25_retriever = BM25Retriever.from_defaults(docstore=docstore, similarity_top_k=5)
